2f5872dd88f1b6db86cc2639a0995d2bf2ba5fdc
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-17 00:44:46 +0100

    Corrected 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <sys/syscall.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -9,9 +9,8 @@
 
 int main(void)
 {
+	char *n = "that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	const char msg[] = "and that piece of art is useful\""
-		"- Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg));
-	return (0);
+	write(STDERR_FILENO, n, strlen(n));
+	return (1);
 }

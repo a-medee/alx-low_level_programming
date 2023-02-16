a849ab34a4b5ab3c15b9141d94203324cb4ae977
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-17 00:03:48 +0100

    Added 101-quote.c; advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+
+	const char msg[] = "and that piece of art is useful\""
+		"- Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (0);
+}

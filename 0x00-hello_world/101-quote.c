f5a354a91c15ab622bd21638178ebcf911525b17
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-17 01:49:47 +0100

    Corrected 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,11 @@
  * Return: Always 1 (Success)
  */
 
+
 int main(void)
 {
-	char *n = "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
+
+	char *n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(STDERR_FILENO, n, strlen(n));
 	return (1);

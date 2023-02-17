1884d9919682db20ef6c90a33e83a8d7c7b0f706
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-17 01:53:54 +0100

    Corrected 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 
-	char *n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDERR_FILENO, n, strlen(n));
 	return (1);

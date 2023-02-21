993d328c3367f795dec417b996474487e4677ae0
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-21 17:39:00 +0100

    Corrected 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,7 @@
-#include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
+
 void print_alphabet_x10(void)
 {
 

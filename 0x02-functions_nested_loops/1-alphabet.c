1ee1fb39798efeddb4912e8fc2993d5efd92b198
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-21 16:40:17 +0100

    Added the 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a++);
+	}
+
+	_putchar('\n');
+	return (0);
+}

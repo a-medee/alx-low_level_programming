942945892d58d9d2b22c7d8c5732f896a67a2f70
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-22 02:01:02 +0100

    Adding 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table
+ *
+ */
+
+void times_table() {
+
+	int i, j , res, number=9;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j=0; j <= 10; j++)
+		{
+			res = j * i;
+			if (j != 10)
+			{
+				print_all(res);
+				_putchar(',');
+				_putchar(' ');
+			}
+			print_all(res);
+		}
+
+		_putchar('\n');
+	}
+}
+
+
+/**
+ * print_all - print all number took in as parameter
+ * @n : type int parameter
+ *
+ * Description: a function that display the number passed in parameter
+ */
+
+void print_all(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_all(n / 10);
+
+	_putchar(n % 10 + '0');
+}

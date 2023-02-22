f206958294a9bc7e892f5a228feb35edf07d1df5
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-22 02:24:38 +0100

    Added 10. a+b and its matching header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a - first parameter of type int
+ * @b - second parameter of type int
+ *
+ * description: add two integers took as arguments
+ * Return: it returns an integer
+ */
+
+
+int add(int a, int b)
+{
+	return (a+b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void print_all(int);
+int add(int, int);

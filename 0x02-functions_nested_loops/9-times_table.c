9646aedd600af2fcd4ae3c0c0b9e5478c750d2d3
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-22 02:08:46 +0100

    Changed the loop control variable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			res = j * i;
 
-			if (j != 10)
+			if (j != 9)
 			{
 				print_all(res);
 				_putchar(',');

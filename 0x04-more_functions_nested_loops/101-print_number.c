471ef84ee349b2d7483b064dfabcea87d6b220d3
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-23 19:37:04 +0100

    Changed hard coded value

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,16 @@
 
 void print_number(int n)
 {
-	int p = 10;
-	char c = '0';
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / p)
+	if (n / 10)
 	{
-		print_number(n / p);
+		print_number(n / 10);
 	}
 
-	_putchar(n % p + c);
+	_putchar(n % 10 + '0');
 }

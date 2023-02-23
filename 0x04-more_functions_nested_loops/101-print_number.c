4a0ce0540d25e89aca614ad8ca8d5de6fa54c964
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-23 19:32:37 +0100

    Corrected function call name

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,21 +4,23 @@
  * print_number - print all number took in as parameter
  * @n : type int parameter
  *
- * Description: a function that displays the numbers passed in
  */
 
 void print_number(int n)
 {
+	int p = 10;
+	char c = '0';
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
+	if (n / p)
 	{
-		print_all(n / 10);
+		print_number(n / p);
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar(n % p + c);
 }

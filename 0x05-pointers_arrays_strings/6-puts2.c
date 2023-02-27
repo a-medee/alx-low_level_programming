0779f7f255e0d7464fe5b19e2e77ecb85eef952a
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-27 22:25:53 +0100

    Added 6-puts2.c along with its corresponding main.h version

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: pointer to type char
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i <= strlen(str))
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
+	}
+}

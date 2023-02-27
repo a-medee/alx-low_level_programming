869b0b080f3d548aabd19b9c8ec4f2e0c3c206c9
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-27 22:48:22 +0100

    Added 7-puts_half.c along with its corresponding main.h description

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - half of a string
+ * @str: pointer to type char
+ *
+ * description - the function should print the second half of the string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	unsigned long size = strlen(str);
+	unsigned int i, length_to_go;
+
+	if ((size % 2) == 0)
+	{
+		length_to_go = size / 2;
+		i = length_to_go;
+
+		while (i < size)
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+
+		_putchar('\n');
+	}
+
+	else
+	{
+		length_to_go = (size - 1) / 2;
+		i = length_to_go;
+
+		while (i < size)
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+
+		_putchar('\n');
+	}
+}

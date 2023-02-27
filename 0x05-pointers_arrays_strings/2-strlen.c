4b9788192bc7996009f755dd10d303728a55c93c
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-27 11:25:31 +0100

    Added 2-strlen along with its main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - a function returns the length of a string.
+ * @s: a parameter of type char *
+ *
+ * description: a function that returns the length of a string.
+ * Return: type int value, length of the given array
+ */
+
+int _strlen(char *s)
+{
+	unsigned long size = strlen(s);
+
+	return (size);
+}

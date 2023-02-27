e7c958b23a1ea6b95ff9beb93af64563a7af4fe2
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-02-27 23:31:34 +0100

    Added my own strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies the string
+ * @dest: pointer to type char
+ * @src: pointer to type char
+ *
+ * description -  function that copies the string pointed to by src, including
+ * the terminating null byte o the buffer pointed to by dest
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest,src));
+}

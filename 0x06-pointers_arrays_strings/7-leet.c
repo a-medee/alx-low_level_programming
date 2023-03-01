eff7a0d3f604c76126f8dfa41a8ff4fb90b331bb
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-01 16:51:38 +0100

    I'm a hacker.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * leet - encode a string
+ * @a: a pointer-to-char.
+ *
+ * description: a function that encodes a string into leet
+ * Return: a pointer-to-char
+ */
+
+char *leet(char *str)
+{
+	char tab[] = "oltea";
+	int i = 0, j;
+
+	while (str[i])
+	{
+		j = 0;
+		while (tab[j])
+		{
+			if (tolower(str[i]) == tab[j])
+			{
+				str[i] = '0' + j + 5 * (tab[j] / 116);
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}

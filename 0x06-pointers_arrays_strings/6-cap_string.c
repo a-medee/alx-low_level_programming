5fa23dc51ea754f98c4d07b0d9e1ec0b3feea8d6
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-01 17:28:48 +0100

    Still capitalizing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,18 +19,18 @@ char *cap_string(char *a)
 
 	for (i = 0; i < size; i++)
 	{
-		truth = ((ispunct(*(a + i)) || isspace(*(a + i))));
-		if (*(a + i) == '\t'){
-			*(a + i) = ' ';
+		truth =((ispunct(*(a + i)) || isspace(*(a + i))));
+		if (*(a + i) == '.'){
+			*(a + (i + 1)) = toupper (*(a + (i + 1)));
 		}
-		if (!truth && !inword)
-		{
+		if (!truth && !inword){
 			inword = 1;
 			*(a + i) = toupper(*(a + i));
 		}
 
 		if (isspace(*(a + i)) && inword)
 			inword = 0;
+
 	}
 	return (a);
 }

2f9efe73f2c6b80c9b769dfa3cd14527d84683e2
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-02 02:47:45 +0100

    Firt cut attempt

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rot13 - compares two strings.
+ * @s1: a pointer-to-char.
+ *
+ * description: a function that copies two strings.
+ * Return: a pointer-to-char
+ */
+
+char * rot13(char *s1)
+{
+	unsigned long int a = strlen(s1), i;
+	int o = 26, j = 0 ;
+
+	char * p = "ABCDEFGHIJKLMabcdefghijklm";
+	char * l = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	for (i = 0; i < a; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (*(s1 + i) == (*(p + j)))
+				(*(s1 + i)) = *(l + j);
+			else if (*(s1 + i) == (*(l + j)))
+				*(s1 + i) = *(p + j);
+		}
+	}
+
+	return (s1);
+}

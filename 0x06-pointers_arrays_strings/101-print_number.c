60a603e4371679014fb3f6b60ccdb1778649201c
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-02 04:08:39 +0100

    First cut attempt

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * @n: a type int paramater
+ *
+ * description: a function that prints an integer
+ * Return: type void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}

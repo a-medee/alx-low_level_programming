bd841aadb373464ca3adf1ef7f6d98915fe90bd5
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-11 01:22:38 +0100

    Put the correct file name. Added mandatory 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,11 +10,8 @@
  * Return: Always 0
  */
 
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc __attribute__((unused)), char **argv)
 {
-
-
-	printf("%d\n", argc - 1);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }

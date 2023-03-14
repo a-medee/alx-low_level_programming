8d53fdd07a14ab29fdaeed40b2cce34034148361
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-14 23:36:26 +0100

    It isn't the mountains ahead to climb that wear you out;

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -53,22 +53,3 @@ char *argstostr(int ac, char **av)
 	return (a);
 
 }
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
- int main(int ac, char *av[])
- {
-     char *s;
- 
-     s = argstostr(ac, av);
-     if (s == NULL)
-     {
-         return (1);
-     }
-     printf("%s", s);
-     free(s);
-     return (0);
- }

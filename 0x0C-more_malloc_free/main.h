66973b57adc86bd4353c61a805371d1997af8b51
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-17 00:56:46 +0100

    3. array_range

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
+int *array_range(int min, int max);

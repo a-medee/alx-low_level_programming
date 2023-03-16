03572805b1ae109fa7e0237c184173adc028b367
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-16 19:06:19 +0100

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for array using malloc
+ * @nmemb: the number of bytes to allocate
+ * @size: the size the size in bytes of a single nmemb element
+ *
+ * Return: a type void *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *a;
+	unsigned long i = 0;
+
+	if ((nmemb == 0) || (size == 0))
+	{
+		return (NULL);
+	}
+
+	a = malloc(nmemb * size);
+
+	if (a)
+	{
+		for (i = 0; i < size; i++)
+		{
+			*(a + i) = 0;
+		}
+		return (a);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 char *n_differ(char *, char *, char *, int);
 char *n_issame(char *, char *, char *);
+void *_calloc(unsigned int nmemb, unsigned int size);

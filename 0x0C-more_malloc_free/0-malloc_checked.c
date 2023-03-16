3f647db20e061c51679a053c3d97a980f112a30e
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-16 15:21:37 +0100

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: a type unsigned int formal parameter representing
+ * the size we want to allocate
+ *
+ * Return: type void *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *a = malloc(b * sizeof(unsigned int));
+
+	if (a)
+	{
+		return (a);
+	}
+	else
+	{
+		exit(98);
+	}
+}

90d6975fcf832d008a9c2b7eb69ed60afa72c685
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-16 22:05:57 +0100

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a = NULL;
-	unsigned long c;
+	int count = 0;
+	unsigned long b = strlen(s1), c = strlen(s2), i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -25,42 +26,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	c = strlen(s2);
-	if (n < c)
+	if (n >= c)
 	{
-		a = n_differ(s1, s2, a, n);
+		n = c;
 	}
-	else
-	{
-		a = n_issame(s1, s2, a);
-	}
-
-	return (a);
-}
-
-/**
- * n_differ - form a new string from two stings
- * @s1: a pointer to char
- * @s2: a pointer to char
- * @a: a pointer to the first element of the newly created
- * string
- * @n: the number of bytes to be copied
- *
- * description - append a n bytes copy of s2 onto the end of s1 and
- * put the resulting sting into in a new memory
- * Return: a pointer to the first element of the newly created string
- */
-
-char *n_differ(char *s1, char *s2, char *a, int n)
-{
-	int count = 0;
-	unsigned long b = strlen(s1), c = strlen(s2);
-	unsigned long i = 0;
-
 	a = malloc((b + c + 1) * sizeof(char));
 
 	if (a)
 	{
-		while (i < (b + c + 1))
+		while (i < (b + c))
 		{
 			if (*s1)
 			{
@@ -70,48 +44,6 @@ char *n_differ(char *s1, char *s2, char *a, int n)
 			{
 				*(a + ++i) = *s2++;
 				count++;
-				continue;
-			}
-			i++;
-		}
-		*(a + i) = '\0';
-		return (a);
-	}
-	else
-	{
-		return (NULL);
-	}
-}
-
-/**
- * n_issame - form a new string from two stings if
- * @s1: a pointer to char
- * @s2: a pointer to char
- * @a: the resulting string
- *
- * description - append a copy of s2 onto the end of s1 and
- * put the resulting sting into in a new memory
- * Return: a pointer to the first element of the newly created string
- */
-
-char *n_issame(char *s1, char *s2, char *a)
-{
-	unsigned long b = strlen(s1), c = strlen(s2);
-	unsigned long i = 0;
-
-	a = malloc((b + c + 1) * sizeof(char));
-
-	if (a)
-	{
-		while (i < (b + c + 1))
-		{
-			if (*s1)
-			{
-				*(a + i) = *s1++;
-			}
-			if (!(*s1) && *s2)
-			{
-				*(a + ++i) = *s2++;
 				continue;
 			}
 			i++;

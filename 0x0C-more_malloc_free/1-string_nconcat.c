ef2663a9c6a54b92dde43cf7c60d420e86b3f207
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-16 17:38:31 +0100

    1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,125 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - form a new string from two stings
+ * @s1: a pointer to char
+ * @s2: a pointer to char
+ * @n: the number of bytes to be copied
+ *
+ * description - append a n bytes copy of s2 onto the end of s1 and
+ * put the resulting sting into in a new memory
+ * Return: a pointer to the first element of the newly created string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a = NULL;
+	unsigned long c;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	c = strlen(s2);
+	if (n < c)
+	{
+		a = n_differ(s1, s2, a, n);
+	}
+	else
+	{
+		a = n_issame(s1, s2, a);
+	}
+
+	return (a);
+}
+
+/**
+ * n_differ - form a new string from two stings
+ * @s1: a pointer to char
+ * @s2: a pointer to char
+ * @a: a pointer to the first element of the newly created
+ * string
+ * @n: the number of bytes to be copied
+ *
+ * description - append a n bytes copy of s2 onto the end of s1 and
+ * put the resulting sting into in a new memory
+ * Return: a pointer to the first element of the newly created string
+ */
+
+char *n_differ(char *s1, char *s2, char *a, int n)
+{
+	int count = 0;
+	unsigned long b = strlen(s1), c = strlen(s2);
+	unsigned long i = 0;
+
+	a = malloc((b + c + 1) * sizeof(char));
+
+	if (a)
+	{
+		while (i < (b + c))
+		{
+			if (*s1)
+			{
+				*(a + i) = *s1++;
+			}
+			if (!(*s1) && *s2 && count < n)
+			{
+				*(a + ++i) = *s2++;
+				count++;
+				continue;
+			}
+			i++;
+		}
+		*(a + i) = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+
+	return (a);
+}
+
+/**
+ * n_issame - form a new string from two stings if
+ * @s1: a pointer to char
+ * @s2: a pointer to char
+ * @a: the resulting string
+ *
+ * description - append a copy of s2 onto the end of s1 and
+ * put the resulting sting into in a new memory
+ * Return: a pointer to the first element of the newly created string
+ */
+
+char *n_issame(char *s1, char *s2, char *a)
+{
+	unsigned long b = strlen(s1), c = strlen(s2);
+	unsigned long i = 0;
+
+	a = malloc((b + c + 1) * sizeof(char));
+
+	if (a)
+	{
+		while (i < (b + c))
+		{
+			if (*s1)
+			{
+				*(a + i) = *s1++;
+			}
+			if (!(*s1) && *s2)
+			{
+				*(a + i) = *s2++;
+			}
+			i++;
+		}
+		*(a + i) = '\0';
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,1 +1,4 @@
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+char *n_differ(char *, char *, char *, int);
+char *n_issame(char *, char *, char *);

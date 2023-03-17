11e462fb316debd305a52676457c12ec43ef6960
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-17 01:06:41 +0100

    3. array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - allocate memory for an array of integer whose size is max -min
+ * @min: a type int value
+ * @max: a type int value
+ *
+ * Return: a pointer to the new space of memory allocated
+ * Or NULL if allocation failed
+ */
+
+int *array_range(int min, int max)
+{
+	int *a, i = 0, size = max - min;
+
+	if (min > max)
+		return (NULL);
+
+	a = malloc(sizeof(int) * size);
+
+	if (a)
+	{
+		for (i = 0; i < size || (min != (max + 1)); i++, min++)
+		{
+			*(a + i) = min;
+		}
+
+		return (a);
+	}
+	else
+	{
+		return (NULL);
+	}
+}

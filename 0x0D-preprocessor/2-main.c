1a5165c8e7e47d2edc8698fae6a4b0f1665db656
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-20 18:01:27 +0100

    Added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <bits/types/__FILE.h>
+#include <stdio.h>
+
+/**
+ * main - entry code point
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

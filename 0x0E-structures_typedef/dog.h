73ccc29187e06e1b41d52cd9a1e06bf8ab973f0e
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-20 21:51:06 +0100

    Added dog.h header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - create a new type named dog
+ * @name: a type char * variable
+ * @age: a type int variable
+ * @owner: a type char * variable
+ *
+ * Description: a structure that creates a dog type using 3 fieds
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */

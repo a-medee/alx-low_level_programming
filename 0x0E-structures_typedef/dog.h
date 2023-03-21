2247e252d312160dc794d36b21879f42da0bfdf4
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-03-21 01:29:32 +0100

    Dog.h to its previous state

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,11 @@ struct dog
 	char *owner;
 };
 
-
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void copy_into(char *str, char *dest);
-typedef struct dog dog_t;
 void free_dog(dog_t *);
 dog_t *new_dog(char *name, float age, char *owner);
 

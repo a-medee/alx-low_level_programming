cc9e62ebaee627f1e11850c164e724c1bbf6ef33
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-04-03 20:50:28 +0100

    Being Betty compliant on the 9th file

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -  a function that returns the nth node of a
+ * sum_listint - a function that returns the nth node of a
  * listint_t linked list.
  * @head: a pointer to the first element of our linked list
  *
@@ -14,7 +14,7 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *firstnode = head;
 
-        while (head)
+	while (head)
 	{
 		sum += head->n;
 		head = head->next;

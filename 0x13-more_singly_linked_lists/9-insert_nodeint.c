e524e6cdb7b25da8e0a382ef91306febe32af2e8
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-04-03 23:10:33 +0100

    Being betty compliant

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,14 +20,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int size = (unsigned int)listint_len(*head);
 	listint_t *nodeidx = malloc(sizeof(listint_t)), *beforenode, *afternode;
 
-        if  ((*head) && (idx == 0) && nodeidx)
+	if  ((*head) && (idx == 0) && nodeidx)
 	{
 		nodeidx->next = (*head);
 		nodeidx->n = n;
 		(*head) = nodeidx;
 		return (nodeidx);
 	}
-        if  ((*head) && (idx <= size) && nodeidx)
+
+	if  ((*head) && (idx <= size) && nodeidx)
 	{
 		afternode = get_nodeint_at_index(*head, idx);
 		beforenode = get_nodeint_at_index(*head, idx - 1);
@@ -87,35 +88,4 @@ size_t listint_len(const listint_t *h)
 		h = h->next;
 	}
 	return (count);
-}
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    printf("-----------------\n");
-    insert_nodeint_at_index(&head, 0, 4096);
-    print_listint(head);
-    free_listint2(&head);
-    return (0);
 }

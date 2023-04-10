97ab40c59a2aaad3104ddf0dc223d2f48cd71b31
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-04-10 09:40:00 +0100

    Being betty compliant on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-                if (file == -1)
+		if (file == -1)
 			return (-1);
 
 		if (text_content)

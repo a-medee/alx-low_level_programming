7bccfc2bd45eedc4623e5b575ab1a7c01d12dcdb
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-04-11 20:24:50 +0100

    Being betty-compliant

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -16,6 +16,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
 
-        (*n) |= (1 << index);
+	(*n) |= (1 << index);
 	return (1);
 }

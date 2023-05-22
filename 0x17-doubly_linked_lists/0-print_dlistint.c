0fbcd474be9a445b135e117c43f310478d72dbd2
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-05-22 14:53:27 +0100

    corrected the displaying function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	ssize_t count = 0;
+	dlistint_t *first_t = h;
 
 	while (h)
 	{
@@ -18,5 +19,7 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
+	h = first_t;
+
 	return (count);
 }

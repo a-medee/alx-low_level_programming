8b4221bbd8bd10b5177ec809b6f0e7e0aed3226e
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-09 14:01:28 +0100

    added an insert function

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,71 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set -  function that adds an element
+ * to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key
+ * @value: is the value associated with the key.
+ *
+ * Return: 1 if succeeded 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+	unsigned long int _keyindex;
+
+	if (node && ht)
+	{
+		_keyindex = key_index((const unsigned char *)key, ht->size);
+
+		if (!ht->array[_keyindex])
+		{
+			node->key = strdup(key);
+			node->value = strdup(value);
+			ht->array[_keyindex] = node;
+
+			return (1);
+		}
+		else
+		{
+			node = add_node(&node, key, value);
+			if (node)
+			{
+				ht->array[_keyindex] = node;
+				return (1);
+			}
+			return (0);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * add_node - function that adds a new node at the beginning of a
+ * hash_node_t list
+ * @head: the head of the list
+ * @key: is the key
+ * @value: is the value associated with the key @key
+ *
+ * Return: the address of the new element, or NULL if it failed;
+ */
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *ad = malloc(sizeof(hash_node_t));
+
+	if (ad)
+	{
+		ad->key = strdup(key);
+		ad->value = strdup(value);
+		ad->next = *head;
+
+		(*head) = ad;
+		return (ad);
+	}
+
+	return (NULL);
+}

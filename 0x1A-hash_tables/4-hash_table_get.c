4a905437f1d77651cb6392cb8bdf782a3fb7f0b4
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-10 05:18:44 +0100

    check for when ht is null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,6 +21,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	}
 
+	if (!ht)
+	{
+		return (NULL);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (!ht->array[index])

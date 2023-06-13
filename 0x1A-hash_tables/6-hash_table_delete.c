edd43083f92609d73853d3d88001df81d9d3bba9
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-13 13:28:41 +0100

    modified delete function

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,53 +1,37 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
- * hash_table_delete - a function that deletes a hash table.
- * @ht: is the hash table
- *
- * Return: a type void element
+ * hash_table_delete - deletes the hash table.
+ * @ht: the hash table
+ * Return: void
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long i;
+	hash_node_t *item, *temp;
+	unsigned long int i;
 
 	if (ht && ht->array)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
-			free_list(&ht->array[i]);
+			item = ht->array[i];
+			if (item)
+			{
+				while (item)
+				{
+					temp = item->next;
+					free(item->key);
+					free(item->value);
+					free(item);
+					item = temp;
+				}
+			}
 		}
-
 		free(ht->array);
 		free(ht);
 	}
 	else if (ht && !ht->array)
 	{
 		free(ht);
-	}
-}
-
-/**
- * free_list - free a linked list
- * @head: the head of the list to be freed
- *
- * Return: a type void
- */
-
-void free_list(hash_node_t **head)
-{
-	hash_node_t *torm;
-
-	if (head)
-	{
-		while (*head)
-		{
-			torm = *head;
-			(*head) = (*head)->next;
-			free(torm->key);
-			free(torm->value);
-			free(torm);
-		}
 	}
 }
